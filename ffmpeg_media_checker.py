273a2f247271f0fd1ee371b7fe9e46d058c6f9c6
Jerome <hongyong.lim@dpodium.com>
2023-06-16 16:38:32 +0800

    Code refactoring done and Lambda implementation made

--- ffmpeg_media_checker.py
+++ ffmpeg_media_checker.py
@@ -1,33 +1,90 @@
-#  This module uses ffmpeg as a tool to validate media files. There are multiple layers of validation to fend against primarily spoofing and accidental corruption.
-import ffmpeg
+import subprocess
 
 
-def validate_file(file_path):
-    # Level 1: Check file extension, magic number
-    try:
-        probe = ffmpeg.ffprobe(file_path)
-    except ffmpeg.Error as e:
-        print(f"Level 1 validation failed with error: {e.stderr.decode()}")
+def validate_image(image_path):
+    # Level 1: File extension and magic number check
+    allowed_extensions = ["jpg", "jpeg", "png", "gif", "webp"]
+    file_extension = image_path.lower().rsplit(".", 1)[-1]
+    if file_extension not in allowed_extensions:
+        print("Invalid file extension")
         return False
 
-    # Level 2: Perform operation unique to intended file type
     try:
-        (
-            ffmpeg.input(file_path)
-            .output("null:", vf="hflip", f="null")  # discards output, test only
-            .run(capture_stdout=True, capture_stderr=True)
-        )
-    except ffmpeg.Error as e:
-        print(f"Level 2 validation failed with error: {e.stderr.decode()}")
+        # to check if the file can be decoded/ incl. checking magic number internally
+        cmd = ["ffmpeg", "-v", "error", "-i", image_path, "-f", "null", "-"]
+        subprocess.run(cmd, check=True, capture_output=True)
+        first_check = True
+    except subprocess.CalledProcessError as e:
+        print(f"Invalid image file: {e.stderr.decode().strip()}")
         return False
 
-    # Level 3: Additioanl checks for image files (if necessary)
-    # if probe["streams"][0]["codec_type"] == "video":
-    return True
+    # Level 2: hflip/crop check (example)
+    try:
+        cmd = [
+            "ffmpeg",
+            "-i",
+            image_path,
+            "-vf",
+            "hflip,crop=100:100",
+            "-f",
+            "null",
+            "-",
+        ]
+        subprocess.run(cmd, check=True, capture_output=True)
+        second_check = True
+    except subprocess.CalledProcessError:
+        print("Image failed hflip/crop check")
+        return False
+
+    # Level 3: TBD
+
+    # If all checks pass, the image is considered valid
+    if first_check and second_check:
+        return True
 
 
-filepath = "test_samples/corrupted_1mb_oriwebp.webp"
-final_slash_i = filepath.rfind("/")
+def validate_video(video_path):
+    # Level 1: File extension and magic number check
+    allowed_extensions = ["mp4", "3gp", "mov", "wmv", "avi", "flv"]
+    file_extension = video_path.lower().rsplit(".", 1)[-1]
+    if file_extension not in allowed_extensions:
+        print("Invalid file extension.")
+        return False
 
-if validate_file(filepath):
-    print(f"REPORT\n{filepath[final_slash_i+1:]} is a valid file.")
+    try:
+        # Check if the video can be decoded/incl. magic number check
+        cmd = ["ffprobe", "-v", "error", "-show_format", "-show_streams", video_path]
+        subprocess.run(cmd, check=True, capture_output=True)
+        first_check = True
+    except subprocess.CalledProcessError as e:
+        print(f"Invalid video file: {e.stderr.decode().strip()}")
+        return False
+
+    # Level 2: Video manipulation check (example: flip/cut/pause)
+    try:
+        cmd = ["ffmpeg", "-i", video_path, "-vf", "hflip", "-f", "null", "-"]
+        subprocess.run(cmd, check=True, capture_output=True)
+        second_check = True
+    except subprocess.CalledProcessError:
+        print("Video failed manipulation check")
+        return False
+
+    # Level 3: TBD
+
+    # If all checks pass, the video is considered valid
+    if first_check and second_check:
+        return True
+
+
+# Video Check
+# video_path = "test_samples/output.MOV"
+video_path = "test_samples/vanilla2.3gp"
+if validate_video(video_path):
+    print("Video is valid")
+
+# Image Check
+# image_path = "test_samples/ezgif-2-4222e09cff-orijpg.gif"
+# image_path = "test_samples/corrupted_1mb_oriwebp.webp"
+image_path = "test_samples/1500kb.webp"
+if validate_image(image_path):
+    print("Image is valid")
